a5d88c4c8beb07e477f67f01ee7e6cef4177bd9a
SoffaStayl <moiseenko01@mail.ru>
2023-10-07 20:44:38 +0700

    домашняя работа 2

---
+++ hw-2.js
@@ -0,0 +1,38 @@
+let a = 10;
+alert (a);
+a=alert ("20")
+let iphone = 2007;
+alert (iphone);
+let b = 'Brendon';
+alert (b);
+let g = 10;
+let c = 2;
+ alert ( sum = g + c);
+ alert ( difference = g - c);
+ alert ( work = g * c);
+ alert ( private = g / c);
+ let square = 2;
+ let result = 2 ** 5;
+ alert (`${result}`);
+ let d = 9;
+ let e = 2;
+ alert ( f = 9 % 2); 
+ let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num -= 1;
+alert(num);
+let age = prompt ("Сколько вам лет?");
+alert (age);
+const user = {
+    name: 'Sofa',
+    age: 21,
+    isAdmin: true
+}
+let info = ("Что вы хотите узнать о пользователе?", "name");
+alert (user[info]);
+let m = prompt("Ваше имя?");
+alert (`Привет, ${m}!`)
