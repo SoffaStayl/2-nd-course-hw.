86f63a490b04f46dfb27c64eb5cef7698439b2ac
SoffaStayl <moiseenko01@mail.ru>
2023-10-10 19:26:35 +0700

    Доработка дз

--- hw-2.js
+++ hw-2.js
@@ -1,29 +1,30 @@
 let a = 10;
 alert (a);
-a=alert ("20")
-let iphone = 2007;
+a = 20;
+alert (20); 
+const iphone = 2007;
 alert (iphone);
-let b = 'Brendon';
+const b = 'Brendon';
 alert (b);
 let g = 10;
 let c = 2;
- alert ( sum = g + c);
- alert ( difference = g - c);
- alert ( work = g * c);
- alert ( private = g / c);
+ alert (g + c);
+ alert (g - c);
+ alert (g * c);
+ alert (g / c);
  let square = 2;
  let result = 2 ** 5;
- alert (`${result}`);
+ alert (result);
  let d = 9;
  let e = 2;
- alert ( f = 9 % 2); 
+ alert (d % e); 
  let num = 1;
 num += 5;
 num -= 3;
 num *= 7;
 num /= 3;
 num++;
-num -= 1;
+num--;
 alert(num);
 let age = prompt ("Сколько вам лет?");
 alert (age);
@@ -32,6 +33,9 @@ const user = {
     age: 21,
     isAdmin: true
 }
+user.cityofresidence ="Кемерово";
+user.age = 25;
+delete user.cityofresidence;
 let info = ("Что вы хотите узнать о пользователе?", "name");
 alert (user[info]);
 let m = prompt("Ваше имя?");
