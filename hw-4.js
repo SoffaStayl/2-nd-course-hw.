6618fcd054654e49ed12a066e50ff224bf36f996
SoffaStayl <moiseenko01@mail.ru>
2023-10-19 00:06:36 +0700

    доработка

--- hw-4.js
+++ hw-4.js
@@ -4,6 +4,7 @@ while (i <= n) {
     alert('Привет');
     i++;
 }
+let a = 1;
 while (a <= 5) {
     console.log (a);
     a++;
@@ -13,11 +14,12 @@ let obj = { 'Коля': 200, 'Вася':300, 'Петя':400};
 for (let key in obj) {
     document.write (key + 'зарплата' + obj[key] + 'долларов');
 }
-for (let n = 1000; num < 50; num = 1000 ) {
-    Sistem.out.println(num);
+for (let n = 1000; num < 50; num++ ) {
+    if (num = i / 2) 
+        console.log (num) 
 }
 let friday = 1;
-for (let dayNumber = friday; dayNumber <= 31; dayNumber++) {
+for (let dayNumber = friday; dayNumber <= 31; dayNumber+=7) {
     if(dayNumber % 7 == 0) {
         console.log (`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
         break;
