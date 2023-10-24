82f24341919949114a595168e1b2c02ac8e2316b
SoffaStayl <moiseenko01@mail.ru>
2023-10-24 22:37:58 +0700

    доработка

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function Season () {
     let game = prompt ('Введите номер месяца');
-    if (game = 12 || 1 || 2){
-    return "Зима"
-    } else if (game = 3 || 4 || 5) {
+    if (game == 12 || game == 1 || game == 2) {
+    return "Зима";
+    } else if (game == 3 || game == 4 || game == 5) {
         return "Весна";
-    } else if (game = 6 || 7 || 8) {
+    } else if (game == 6 || game == 7 || game == 8) {
         return "Лето";
-    } else if (game = 9 || 10 || 11) {
-        return "Осень";
-    }
+    } else if (game == 9 || game == 10 || game == 11) {
+       return "Осень";
+    } 
 }
 // alert (Season);
