6f12748af0fa9daa492e075eebc77792d8b5009b
SoffaStayl <moiseenko01@mail.ru>
2023-10-29 12:13:30 +0700

    доработка

--- hw-6.js
+++ hw-6.js
@@ -1,15 +1,15 @@
-const numbs = [1, 5, 4, 10, 0, 3];
+const numbs = [1, 5, 4, 10, 0, 3]; //задание 1
 for (let i = 0; i < numbs.length; i++ ) {
     if (numbs [i] === 10) break;
     console.log(numbs[i]);
-}
+} 
 
 
-const arr = [1, 5, 4, 10, 0, 3];
-console.log(arr.indexOf(4));
+const arr = [1, 5, 4, 10, 0, 3]; //задание 2
+console.log(arr.indexOf(4)); 
 
 
-const a = [1, 3, 5, 10, 20];
+const a = [1, 3, 5, 10, 20]; //задание 3
 console.log(a.join (''));
 const even = [];
 for (let b = 1; b++;) {
@@ -17,10 +17,10 @@ for (let b = 1; b++;) {
 for (let c = 1; c++;) {
     even[b][c];
 }
-}console.log(even);
+}console.log(even); 
 
 
-const sun = [1, 1, 1];
+const sun = [1, 1, 1]; //задание 4
 for (let k = 0; i < 3; k++){
     sun [k] = [];
     for ( let j = 0; j++) {
@@ -29,17 +29,17 @@ for (let k = 0; i < 3; k++){
 }
 console.log(sun);
 
-const owl = [1, 1, 1];
+const owl = [1, 1, 1]; //задание 5
 owl.push (2, 2, 2);
 console.log(owl);
 
-const world = [9, 8, 7, 'a', 6, 5];
+const world = [9, 8, 7, 'a', 6, 5]; //задание 6
 world = world.sort();
 world = world.pop ()
 console.log(world);
 
 
-const quiz = [9, 8, 7, 6, 5];
+const quiz = [9, 8, 7, 6, 5]; //задание 7
 let d = prompt('Угадай число');
 d = quiz.includes;
 if (d === true){
@@ -50,30 +50,30 @@ if (d === true){
 
 
 
-const str = 'abcdef';
+const str = 'abcdef'; //задание 8
 console.log(str.split('').reverse().join(''));
 
 
-const lion = [[1, 2, 3,], [4, 5, 6]];
+const lion = [[1, 2, 3,], [4, 5, 6]]; //задание 9
 const flat = lion.flat();
 console.log(flat);
 
 
-const sum = [1, 3, 6, 5, 7];
+const sum = [1, 3, 6, 5, 7]; //задание 10
 for (let f = 0; f < sum.length; f++) {
    const numbers = sum + sum [f + 1];
 }
 console.log(numbers);
 
 
-const products = [1, 6, 5, 8, 2, 3];
+const products = [1, 6, 5, 8, 2, 3]; //задание 11
 const squares = products.map(function (num){ 
     return num * num
 })
 console.log(squares);
 
 
-const getLenghtwords = [ 'слово', '', 'слог', 'длинное предложение', 'буква'] ;
+const getLenghtwords = [ 'слово', '', 'слог', 'длинное предложение', 'буква'] ; //задание 12
 function lenghtword(get){
 let len = [];
 let word = get.split(' ');
@@ -85,7 +85,7 @@ return len;
 console.log(lenghtword(getLenghtwords) );
 
 
-const negative = [1, 0, -10, 6, -150, -35, 5, -6];
+const negative = [1, 0, -10, 6, -150, -35, 5, -6]; //задание 13
 function filterPositive(array){
     const array = negative.filter (el => el < 0);
     return array;
